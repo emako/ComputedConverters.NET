d091f3ecf847792bc931a36d7dcdd4226c3a9416
ema <mccoy39082@163.com>
2024-08-25 03:37:52 +0800

    feat: Unbinding

---
+++ ComputedConverters.WPF/Markups/UnbindingExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Xaml;
+
+namespace ComputedConverters;
+
+[MarkupExtensionReturnType(typeof(object))]
+public sealed class UnbindingExtension(object? resourceKey) : MarkupExtension
+{
+    [ConstructorArgument("resourceKey")]
+    public object? ResourceKey { get; set; } = resourceKey;
+
+    public UnbindingExtension() : this(default)
+    {
+    }
+
+    public override object? ProvideValue(IServiceProvider serviceProvider)
+    {
+        if (ResourceKey is null)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+        else if (ResourceKey is Binding binding)
+        {
+            if (serviceProvider.GetService(typeof(IProvideValueTarget)) is IRootObjectProvider provideValueTarget)
+            {
+                if (provideValueTarget.RootObject is FrameworkElement targetObject)
+                {
+                    if (targetObject.DataContext is null)
+                    {
+                        return DependencyProperty.UnsetValue;
+                    }
+
+                    string propertyPath = binding.Path.Path;
+
+                    if (targetObject.DataContext.GetType().GetProperty(propertyPath) is PropertyInfo propInfo)
+                    {
+                        return propInfo.GetValue(targetObject.DataContext);
+                    }
+                }
+            }
+        }
+
+        return DependencyProperty.UnsetValue;
+    }
+}
