980bbf862defa18ae4bc8576010f4840739c2c41
ema <mccoy39082@163.com>
2024-08-25 03:07:10 +0800

    feat: ValueConverterGroup

---
+++ ComputedConverters.WPF/Converters/ValueConverterGroup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace ComputedConverters;
+
+[ContentProperty(nameof(Converters))]
+public sealed class ValueConverterGroup : IValueConverter
+{
+    public List<IValueConverter> Converters { get; set; } = [];
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (Converters is IEnumerable<IValueConverter> converters)
+        {
+            return converters.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+        }
+
+        return DependencyProperty.UnsetValue;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (Converters is IEnumerable<IValueConverter> converters)
+        {
+            return converters.Reverse().Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+        }
+
+        return DependencyProperty.UnsetValue;
+    }
+}
