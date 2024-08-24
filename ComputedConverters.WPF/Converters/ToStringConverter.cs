da44f4f3c26804d99c3c394dc5b618bc58ce577f
ema <mccoy39082@163.com>
2024-08-25 03:06:56 +0800

    feat: ToStringConverter support override

--- ComputedConverters.WPF/Converters/ToStringConverter.cs
+++ ComputedConverters.WPF/Converters/ToStringConverter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
+using System.Reflection;
 using System.Windows.Data;
 
 namespace ComputedConverters;
@@ -9,6 +11,16 @@ public sealed class ToStringConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
+        if (value != null && parameter != null)
+        {
+            return value.GetType()
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                .Where(m => m.Name == nameof(ToString))
+                .Where(m => m.GetParameters().Length == 1 && m.GetParameters().First().ParameterType == parameter.GetType())
+                .FirstOrDefault()
+                ?.Invoke(value, [parameter]);
+        }
+
         return value?.ToString();
     }
 
