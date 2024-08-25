ca40b9ed99129e3926f11173fc3cfdb6e8ee112a
ema <mccoy39082@163.com>
2024-08-26 03:15:02 +0800

    fix: ChangeTypeConverter

--- ComputedConverters.WPF/Converters/ChangeTypeConverter.cs
+++ ComputedConverters.WPF/Converters/ChangeTypeConverter.cs
@@ -8,27 +8,22 @@ namespace ComputedConverters;
 [ValueConversion(typeof(object), typeof(object))]
 public sealed class ChangeTypeConverter : SingletonValueConverterBase<ChangeTypeConverter>
 {
+    public Type? TargetType
+    {
+        get => (Type?)GetValue(TargetTypeProperty);
+        set => SetValue(TargetTypeProperty, value);
+    }
+
+    public static readonly DependencyProperty TargetTypeProperty =
+        DependencyProperty.Register(nameof(TargetType), typeof(Type), typeof(ChangeTypeConverter), new PropertyMetadata(null));
+
     public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        try
-        {
-            return System.Convert.ChangeType(value, targetType);
-        }
-        catch
-        {
-            return DependencyProperty.UnsetValue;
-        }
+        return System.Convert.ChangeType(value, TargetType ?? targetType);
     }
 
     public override object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        try
-        {
-            return System.Convert.ChangeType(value, targetType);
-        }
-        catch
-        {
-            return DependencyProperty.UnsetValue;
-        }
+        return System.Convert.ChangeType(value, TargetType ?? targetType);
     }
 }
