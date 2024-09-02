cfe227b07dcf3728eef0710a54059c2fa237147a
ema <mccoy39082@163.com>
2024-09-03 00:55:13 +0800

    feat: EnumLocaleDescriptionConverter

--- ComputedConverters.WPF.Test/MainViewModel.cs
+++ ComputedConverters.WPF.Test/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
 using System.Windows;
 
 namespace ComputedConverters.Test;
@@ -24,7 +25,43 @@ public partial class MainViewModel : ObservableObject
     [ObservableProperty]
     public bool isLoading = true;
 
+    [ObservableProperty]
+    public TestEnum testEnumValue = TestEnum.Second;
+
+    [ObservableProperty]
+    public TestLocaleEnum testLocaleEnumValue = TestLocaleEnum.Second;
+
     public MainViewModel()
     {
     }
+}
+
+public enum TestEnum
+{
+    [Description("First")]
+    First = 1,
+
+    [Description("Second")]
+    Second = 2,
+
+    [Description("Third")]
+    Third = 3
+}
+
+public enum TestLocaleEnum
+{
+    [LocaleDescription("en", "First", isFallback: true)]
+    [LocaleDescription("ja", "ファースト")]
+    [LocaleDescription("zh", "第一个")]
+    First = 1,
+
+    [LocaleDescription("en", "Second", isFallback: true)]
+    [LocaleDescription("ja", "セカンド")]
+    [LocaleDescription("zh", "第二个")]
+    Second = 2,
+
+    [LocaleDescription("en", "Third", isFallback: true)]
+    [LocaleDescription("ja", "サード")]
+    [LocaleDescription("zh", "第三个")]
+    Third = 3
 }
---
+++ ComputedConverters.WPF/Attributes/LocaleDescriptionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ComputedConverters;
+
+[AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+public class LocaleDescriptionAttribute(string locale, string description, bool isFallback = false) : Attribute
+{
+    private string locale = locale;
+    public virtual string Locale => locale;
+
+    private string description = description;
+    public virtual string Description => description;
+
+    private bool isFallback = isFallback;
+    public virtual bool IsFallback => isFallback;
+
+    public LocaleDescriptionAttribute() : this(string.Empty, string.Empty)
+    {
+    }
+
+    public LocaleDescriptionAttribute(string description) : this(string.Empty, description)
+    {
+    }
+}
