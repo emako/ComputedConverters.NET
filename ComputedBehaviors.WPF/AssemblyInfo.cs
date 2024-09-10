5fdb1e48a76c44eee385bc521d46368d5804938a
ema <mccoy39082@163.com>
2024-09-10 16:21:07 +0800

    feat: sln ComputedBehaviors

---
+++ ComputedBehaviors.WPF/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("http://schemas.github.com/computedbehaviors/2024/xaml", "b")]
+[assembly: XmlnsDefinition("http://schemas.github.com/computedbehaviors/2024/xaml", "ComputedBehaviors")]
