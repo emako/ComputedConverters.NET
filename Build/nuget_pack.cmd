200c48fae3c388a5c350024eb68ff1c8b11634f8
ema <mccoy39082@163.com>
2024-09-19 18:19:39 +0800

    fix: lost build before pack

--- Build/nuget_pack.cmd
+++ Build/nuget_pack.cmd
@@ -1,13 +1,16 @@
 cd /d %~dp0
 cd /d ..\ComputedAnimations.WPF
 dotnet restore
+dotnet build -c Release
 dotnet pack -c Release -o ../Build/
 cd /d %~dp0
 cd /d ..\ComputedBehaviors.WPF
 dotnet restore
+dotnet build -c Release
 dotnet pack -c Release -o ../Build/
 cd /d %~dp0
 cd /d ..\ComputedConverters.WPF
 dotnet restore
+dotnet build -c Release
 dotnet pack -c Release -o ../Build/
 @pause
