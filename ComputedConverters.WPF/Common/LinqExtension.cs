2f6a99ed0e8e337ca9e0af796fc40e9a860cf401
ema <mccoy39082@163.com>
2024-09-19 09:06:24 +0800

    feat: Common methods

---
+++ ComputedConverters.WPF/Common/LinqExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ComputedConverters;
+
+public static class LinqExtension
+{
+    public static void ForEach<TSource>(this IEnumerable<TSource> ts, Action<TSource> action)
+    {
+        foreach (TSource t in ts)
+        {
+            action(t);
+        }
+    }
+
+    public static void ForEach<TSource>(this IEnumerable<TSource> ts, Action<TSource, int> action)
+    {
+        int i = default;
+        foreach (TSource t in ts)
+        {
+            action(t, i);
+            i++;
+        }
+    }
+
+    public static async Task ForEachAsync<TSource>(this IEnumerable<TSource> ts, Func<TSource, Task> action)
+    {
+        foreach (TSource t in ts)
+        {
+            await action(t);
+        }
+    }
+
+    public static async Task ForEachAsync<TSource>(this IEnumerable<TSource> ts, Func<TSource, int, Task> action)
+    {
+        int i = default;
+        foreach (TSource t in ts)
+        {
+            await action(t, i);
+            i++;
+        }
+    }
+}
---
+++ ComputedConverters.WPF/Common/SmartEnum{T}.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace ComputedConverters;
+
+public abstract class SmartEnum<TEnum> where TEnum : struct
+{
+    public static TEnum? FromName(string name, TEnum? @default = default)
+    {
+        if (Enum.TryParse(name, out TEnum @enum))
+        {
+            return @enum;
+        }
+        return @default;
+    }
+
+    public static TEnum? FromValue(int? value, TEnum? @default = default)
+    {
+        if (value != null)
+        {
+            if (Enum.TryParse(value.ToString(), out TEnum @enum))
+            {
+                return @enum;
+            }
+        }
+        return @default;
+    }
+
+    public static TEnum? FromDescriptionAttribute(string? description, TEnum? @default = default)
+    {
+        if (description != null)
+        {
+            foreach (TEnum enumValue in Enum.GetValues(typeof(TEnum)))
+            {
+                MemberInfo enumMember = typeof(TEnum).GetMember(enumValue.ToString()!).First();
+
+                if (enumMember.GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault() is DescriptionAttribute attribute && attribute.Description == description)
+                {
+                    return enumValue;
+                }
+            }
+        }
+        return @default;
+    }
+
+    public static string? ToDescriptionAttribute(TEnum? value, string? @default = default)
+    {
+        if (value != null)
+        {
+            MemberInfo enumMember = typeof(TEnum).GetMember(value.ToString()!).First();
+
+            if (enumMember.GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault() is DescriptionAttribute attribute)
+            {
+                return attribute.Description;
+            }
+        }
+        return @default;
+    }
+}
