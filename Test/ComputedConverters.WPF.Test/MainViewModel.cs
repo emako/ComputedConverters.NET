d5b720da6a47c594b0bf57900c4e5411cd7333d2
ema <mccoy39082@163.com>
2024-09-25 10:51:50 +0800

    test: BitmapToImageSourceConverter

--- Test/ComputedConverters.WPF.Test/MainViewModel.cs
+++ Test/ComputedConverters.WPF.Test/MainViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.ComponentModel;
+using System.IO.Packaging;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Resources;
 using Wpf.Ui.Violeta.Controls;
 
 namespace ComputedConverters.Test;
@@ -74,6 +78,9 @@ public partial class MainViewModel : ReactiveObject
             }
         }
     }
+
+    [ObservableProperty]
+    private System.Drawing.Bitmap bitmap = new(ResourceHelper.GetStream("pack://application:,,,/ComputedConverters.Test;component/test.png"));
 }
 
 public class StaticClass
@@ -117,5 +124,37 @@ public sealed class ServiceLocatorTestPage : UserControl
     public ServiceLocatorTestPage()
     {
         AddChild(new TextBlock { Text = "I'm a page named ServiceLocatorTestPage" });
+    }
+}
+
+public static class ResourceHelper
+{
+    static ResourceHelper()
+    {
+        if (!UriParser.IsKnownScheme("pack"))
+            _ = PackUriHelper.UriSchemePack;
+    }
+
+    public static byte[] GetBytes(string uriString)
+    {
+        Uri uri = new(uriString);
+        StreamResourceInfo info = Application.GetResourceStream(uri);
+        using BinaryReader stream = new(info.Stream);
+        return stream.ReadBytes((int)info.Stream.Length);
+    }
+
+    public static Stream GetStream(string uriString)
+    {
+        Uri uri = new(uriString);
+        StreamResourceInfo info = Application.GetResourceStream(uri);
+        return info?.Stream!;
+    }
+
+    public static string GetString(string uriString, Encoding encoding = null!)
+    {
+        Uri uri = new(uriString);
+        StreamResourceInfo info = Application.GetResourceStream(uri);
+        using StreamReader stream = new(info.Stream, encoding ?? Encoding.UTF8);
+        return stream.ReadToEnd();
     }
 }
