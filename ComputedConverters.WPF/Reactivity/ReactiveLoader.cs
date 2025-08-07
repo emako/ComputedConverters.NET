8abee708c4a2a5994289009ebe382fd7099c187d
ema <mccoy39082@163.com>
2025-08-07 10:46:16 +0800

    feat: ReactiveLoader

---
+++ ComputedConverters.WPF/Reactivity/ReactiveLoader.cs
@@ -0,0 +1,11 @@
+﻿namespace ComputedConverters;
+
+public class ReactiveLoader : Reactive
+{
+    public bool IsInitialized { get; protected set; } = false;
+
+    public void SetInitialized(bool isInitialized)
+    {
+        IsInitialized = isInitialized;
+    }
+}
