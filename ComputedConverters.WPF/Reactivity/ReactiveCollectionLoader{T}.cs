77bfff65059e332837fe7cd79b8171fa9471ddcb
ema <mccoy39082@163.com>
2025-08-07 10:21:39 +0800

    feat: ReactiveCollectionLoader<T>

---
+++ ComputedConverters.WPF/Reactivity/ReactiveCollectionLoader{T}.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ComputedConverters;
+
+public class ReactiveCollectionLoader<T> : ReactiveCollection<T>
+{
+    public bool IsInitialized { get; protected set; } = false;
+
+    public ReactiveCollectionLoader() : base()
+    {
+    }
+
+    public ReactiveCollectionLoader(IEnumerable<T> collection) : base(collection)
+    {
+    }
+
+    public ReactiveCollectionLoader(IList<T> list) : base(list)
+    {
+    }
+
+    public ReactiveCollectionLoader(ICollection<T> collection) : base(collection)
+    {
+    }
+
+    public ReactiveCollectionLoader(IQueryable<T> queryable) : base(queryable)
+    {
+    }
+
+    public void SetInitialized(bool isInitialized)
+    {
+        IsInitialized = isInitialized;
+    }
+}
