a0eff6c6fae2ab10cbf92f73f4169cb931546777
ema <mccoy39082@163.com>
2026-05-18 19:18:47 +0800

    Add Digits dependency property to round converters

--- ComputedConverters.WPF/Converters/DoubleMathRoundConverter.cs
+++ ComputedConverters.WPF/Converters/DoubleMathRoundConverter.cs
@@ -9,17 +9,28 @@ namespace ComputedConverters;
 [ValueConversion(typeof(double), typeof(double))]
 public sealed class DoubleMathRoundConverter : SingletonValueConverterBase<DoubleMathRoundConverter>
 {
+    public static readonly DependencyProperty DigitsProperty =
+        DependencyProperty.Register(nameof(Digits), typeof(int), typeof(DoubleMathRoundConverter), new PropertyMetadata(0));
+
+    public int Digits
+    {
+        get => (int)GetValue(DigitsProperty);
+        set => SetValue(DigitsProperty, value);
+    }
+
     public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         try
         {
             double current = System.Convert.ToDouble(value, culture);
-            int digits = 0;
+            int digits = Digits;
 
+#if false // Breaking change: No longer supported
             if (parameter is not null)
             {
                 digits = System.Convert.ToInt32(parameter, culture);
             }
+#endif
 
             double result = Math.Round(current, digits);
             if (typeof(IConvertible).IsAssignableFrom(targetType))
--- ComputedConverters.WPF/Converters/FloatMathRoundConverter.cs
+++ ComputedConverters.WPF/Converters/FloatMathRoundConverter.cs
@@ -9,17 +9,28 @@ namespace ComputedConverters;
 [ValueConversion(typeof(float), typeof(float))]
 public sealed class FloatMathRoundConverter : SingletonValueConverterBase<FloatMathRoundConverter>
 {
+    public static readonly DependencyProperty DigitsProperty =
+        DependencyProperty.Register(nameof(Digits), typeof(int), typeof(FloatMathRoundConverter), new PropertyMetadata(0));
+
+    public int Digits
+    {
+        get => (int)GetValue(DigitsProperty);
+        set => SetValue(DigitsProperty, value);
+    }
+
     public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         try
         {
             float current = System.Convert.ToSingle(value, culture);
-            int digits = 0;
+            int digits = Digits;
 
+#if false // Breaking change: No longer supported
             if (parameter is not null)
             {
                 digits = System.Convert.ToInt32(parameter, culture);
             }
+#endif
 
             float result = (float)Math.Round(current, digits);
             if (typeof(IConvertible).IsAssignableFrom(targetType))
